22b7406dfb9ae7b0a47624daa957cf195ad7df3f
elnarmen <101528347+elnarmen@users.noreply.github.com>
2022-11-09 13:27:16 +0300

    Move duplicate code to 'functions_for_statistics' file

--- hh_ru_vacancies.py
+++ hh_ru_vacancies.py
@@ -1,10 +1,8 @@
 import requests
-import functools
-import collections
-import operator
+from functions_for_statistics import get_salary_statistics
 
 
-def predict_rub_salary(vacancy: dict):
+def predict_rub_salary_hh(vacancy: dict):
     if vacancy['salary'] is None:
         return None
     salary_details = vacancy['salary']
@@ -12,13 +10,13 @@ def predict_rub_salary(vacancy: dict):
         return None
     if salary_details['from'] and salary_details['to']:
         return (salary_details['from'] + salary_details['to']) / 2
-    elif salary_details['from'] is None:
+    if salary_details['to']:
         return salary_details['to'] * .8
-    elif salary_details['to'] is None:
+    elif salary_details['from']:
         return salary_details['from'] * 1.2
+    return None
 
-
-def get_all_language_vacancies(language: str):
+def get_all_language_vacancies_hh(language: str):
     url = 'https://api.hh.ru/vacancies'
     user_agent = {'User-agent': 'Mozilla/5.0'}
     page = 0
@@ -43,44 +41,7 @@ def get_all_language_vacancies(language: str):
     return all_vacancies, vacancies_found
 
 
-def get_average_language_salaries(vacancies, ammount_of_vacancies):
-    total_salary = 0
-    vacancies_processed = 0
-    for vacancy in vacancies:
-        salary = predict_rub_salary(vacancy)
-        if salary:
-            total_salary += salary
-            vacancies_processed += 1
-    average_salary = None
-    if vacancies_processed:
-        average_salary = int(total_salary / vacancies_processed)
-    salaries_details = {
-        'vacancies_found': ammount_of_vacancies,
-        'vacancies_processed': vacancies_processed,
-        'average_salary': average_salary
-    }
-    return salaries_details
+if __name__ == '__main__':
+    print(get_salary_statistics(get_all_language_vacancies_hh, predict_rub_salary_hh))
 
 
-languages = ['Python', 'C', 'Java', 'C++', 'C#', 'Visual Basic', 'JavaScript', 'PHP', 'Swift', 'Go', 'Kotlin', 'Solidity']
-salary_statistics = {}
-for language in languages:
-    vacancies, ammount_of_vacancies = get_all_language_vacancies(language)
-    salary_statistics[language] = get_average_language_salaries(vacancies, ammount_of_vacancies)
-
-for k, v in salary_statistics:
-    print(f'''Язык программирования: {k}.
-    Вакансий найдено: {v['vacancies_found']}
-    Вакансий обработано: {v['vacancies_processed']}
-    Средняя зарплата: {v['average_salary']}''')
-
-d = {'Python': {'vacancies_found': 1126, 'vacancies_processed': 273, 'average_salary': 199573},
-     'C': {'vacancies_found': 1545, 'vacancies_processed': 539, 'average_salary': 183932},
-     'Java': {'vacancies_found': 1406, 'vacancies_processed': 199, 'average_salary': 231611},
-     'C++': {'vacancies_found': 863, 'vacancies_processed': 246, 'average_salary': 176242},
-     'C#': {'vacancies_found': 727, 'vacancies_processed': 175, 'average_salary': 184329},
-     'Visual Basic': {'vacancies_found': 22, 'vacancies_processed': 6, 'average_salary': 164500},
-     'JavaScript': {'vacancies_found': 1979, 'vacancies_processed': 602, 'average_salary': 181361},
-     'PHP': {'vacancies_found': 894, 'vacancies_processed': 385, 'average_salary': 166756},
-     'Swift': {'vacancies_found': 209, 'vacancies_processed': 53, 'average_salary': 242726},
-     'Go': {'vacancies_found': 438, 'vacancies_processed': 110, 'average_salary': 232594}}
--- super_job_vacancies.py
+++ super_job_vacancies.py
@@ -1,12 +1,57 @@
 import os
 import requests
 from dotenv import load_dotenv
+from functions_for_statistics import get_salary_statistics
+
+def predict_rub_salary_sj(vacancy):
+    if vacancy['currency'] != 'rub':
+        return None
+    if vacancy['payment_from'] and vacancy['payment_to']:
+        return (vacancy['payment_from'] + vacancy['payment_to']) / 2
+    if vacancy['payment_from']:
+        return vacancy['payment_from'] * 1.2
+    if vacancy['payment_to']:
+        return vacancy['payment_to'] * .8
+    return None
 
 
-load_dotenv()
-url = 'https://api.superjob.ru/2.0/vacancies'
-superjob_api_key = os.getenv('SUPERJOB_API_KEY')
-headers = {'X-Api-App-Id': superjob_api_key}
-response = requests.get(url, headers=headers)
-response.raise_for_status()
-print('\n'.join([i['profession'] for i in response.json()['objects']]))
+def get_all_language_vacancies_sj(language):
+    url = 'https://api.superjob.ru/2.0/vacancies'
+    superjob_api_key = os.getenv('SUPERJOB_API_KEY')
+    page = 0
+    pages_numper = 5  # если параметр count в запросе = 100, API выдаст только 5 страниц
+    other_results = True
+    all_vacancies = []
+    vacancies_found = 0
+    while page < pages_numper and other_results:
+        params = {'town': 4,  # id Москвы
+                  'catalogues': 48,  # id каталога "Разработка, программирование"
+                  'count': 100,  # api запрещает запрашивать больше 100 вакансий
+                  'page': page,
+                  'keyword': language,
+                  'period': 0,
+                  }
+        headers = {'X-Api-App-Id': superjob_api_key}
+        page_response = requests.get(url, params, headers=headers)
+        page_response.raise_for_status()
+        decoder_response = page_response.json()
+        all_vacancies.extend(decoder_response['objects'])
+        vacancies_found = decoder_response['total']
+        other_results = decoder_response['more']
+        page += 1
+    return all_vacancies, vacancies_found
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    print(get_salary_statistics(get_all_language_vacancies_sj, predict_rub_salary_sj))
+
+d = {'Python': {'vacancies_found': 38, 'vacancies_processed': 14, 'average_salary': 126577},
+     'C': {'vacancies_found': 11, 'vacancies_processed': 4, 'average_salary': 147000},
+     'Java': {'vacancies_found': 10, 'vacancies_processed': 5, 'average_salary': 208400},
+     'C++': {'vacancies_found': 12, 'vacancies_processed': 4, 'average_salary': 159750},
+     'C#': {'vacancies_found': 3, 'vacancies_processed': 2, 'average_salary': 197500},
+     'JavaScript': {'vacancies_found': 34, 'vacancies_processed': 21, 'average_salary': 118003},
+     'PHP': {'vacancies_found': 21, 'vacancies_processed': 16, 'average_salary': 144245},
+     'Go': {'vacancies_found': 5, 'vacancies_processed': 0, 'average_salary': None},
+     'Kotlin': {'vacancies_found': 4, 'vacancies_processed': 3, 'average_salary': 229000}}
